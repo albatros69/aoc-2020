473a0c92d4a4c63f6d50272fbea074571e353974
Mathieu <albatros69+github@gmail.com>
2020-12-06 09:58:19 +0100

    Commit day 6

---
+++ day-06/customs.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+
+import sys
+
+lines = []
+for line in sys.stdin:
+	lines.append(line.rstrip('\n'))
+
+# Part 1
+# answers = set()
+# Part 2
+answers = set('abcdefghijklmnopqrstuvwxyz')
+yes_count = []
+for l in lines:
+    if l:
+        # answers = answers.union(set(l))
+        answers = answers.intersection(set(l))
+    else:
+        yes_count.append(len(answers))
+        # answers = set()
+        answers = set('abcdefghijklmnopqrstuvwxyz')
+
+if len(answers) > 0:
+    yes_count.append(len(answers))
+
+print(yes_count, sum(yes_count))
