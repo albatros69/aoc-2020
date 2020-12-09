4d0d7a8237b80719ab74234df76d5dbb884fa153
Mathieu <albatros69+github@gmail.com>
2020-12-09 09:33:49 +0100

    Commit day 9

---
+++ day-09/xmas.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python
+
+import collections
+import sys
+from itertools import combinations
+
+lines = []
+for line in sys.stdin:
+	lines.append(int(line.rstrip('\n')))
+
+
+class Decipher():
+	
+	def __init__(self, preamble_length):
+		self.preamble_length = preamble_length
+		self.cursor = 0
+		self.data = []
+	
+	def read_next(self, value):
+		if len(self.data) < self.preamble_length: # Still reading the preamble
+			self.data.append(value)
+			return True
+		else:
+			if self.is_valid(value):
+				self.cursor += 1 # We move the head of the sliding window
+				self.data.append(value) # and add the new valid value at the end
+				return True
+			else:
+				return False
+	
+	def is_valid(self, value):
+		for (a,b) in combinations(self.data[self.cursor:self.cursor+self.preamble_length], 2):
+			if a+b == value:
+				return True
+
+		return False
+	
+
+# Test
+# test = Decipher(5)
+# for l in lines:
+# 	if not test.read_next(int(l)):
+# 		print(l)
+# 		break
+
+# Part 1
+input = Decipher(25)
+for l in lines:
+	if not input.read_next(l):
+		error = l
+		print(error)
+		break
+
+# Part 2
+# Looking for a subset of contiguous values which sum is egal to the invalid value
+for start in range(0, input.cursor):
+	for end in range(1, input.cursor+1):
+		if sum(input.data[start:end]) == error:
+			print(min(input.data[start:end])+max(input.data[start:end]))
+			break
