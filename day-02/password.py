8e7307834e051ad02e4172e1594cb83b50a8e0e9
Mathieu <albatros69+github@gmail.com>
2021-01-16 22:53:57 +0100

    Both parts in one loop

--- day-02/password.py
+++ day-02/password.py
@@ -8,15 +8,16 @@ lines = []
 for line in sys.stdin:
     lines.append(line.rstrip('\n'))
 
-valid=0
+valid_part1, valid_part2=0,0
 for l in lines:
     (i,s,c,pwd) = split("[ :-]+", l)
+
     # Part 1
-    # count = Counter(pwd)
-    # if int(i) <= count[c] <= int(s):
-    #     valid += 1
+    count = Counter(pwd)
+    if int(i) <= count[c] <= int(s):
+        valid_part1 += 1
     # Part 2
     if (pwd[int(i)-1] == c and pwd[int(s)-1] != c) or (pwd[int(i)-1] != c and pwd[int(s)-1] == c):
-        valid +=1
+        valid_part2 +=1
 
-print(valid)
+print("Part 1:", valid_part1, "\nPart 2:", valid_part2)
